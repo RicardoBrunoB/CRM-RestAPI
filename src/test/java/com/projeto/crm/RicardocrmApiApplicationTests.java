24be863f8612b05fc16808f499df3b76a75a10e4
Ricardo Breustedt <ricardobruno@gmail.com>
2023-04-07 14:42:03 -0300

    Criacao do projeto e metodos CREATE e READ

---
+++ src/test/java/com/projeto/crm/RicardocrmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RicardocrmApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
