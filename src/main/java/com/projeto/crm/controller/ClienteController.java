5117bea1996f7367491ebdfee125404db7453ab2
Ricardo Breustedt <ricardobruno@gmail.com>
2023-04-07 14:57:37 -0300

    Criacao dos metodos UPDADE e DELETE

--- src/main/java/com/projeto/crm/controller/ClienteController.java
+++ src/main/java/com/projeto/crm/controller/ClienteController.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -30,5 +34,25 @@ public class ClienteController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public Cliente adicionar(@RequestBody Cliente cliente) {
 		return clienteRepository.save(cliente);
+	}
+	
+	@PutMapping(value="/{id}")
+	public ResponseEntity<Cliente> atualizar(@PathVariable("id") long id,
+	                                      @RequestBody Cliente cliente) {
+	   return clienteRepository.findById(id)
+	           .map(record -> {
+	               record.setNome(cliente.getNome());
+	               Cliente updated = clienteRepository.save(record);
+	               return ResponseEntity.ok().body(updated);
+	           }).orElse(ResponseEntity.notFound().build());
+	}
+	
+	@DeleteMapping(path ={"/{id}"})
+	public ResponseEntity <?> delete(@PathVariable long id) {
+	   return clienteRepository.findById(id)
+	           .map(record -> {
+	               clienteRepository.deleteById(id);
+	               return ResponseEntity.ok().build();
+	           }).orElse(ResponseEntity.notFound().build());
 	}
 }
